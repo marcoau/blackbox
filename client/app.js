96103ead91eb6f29eadda479b3fad0b03f80060e
Marco Au <marco.au.321@gmail.com>
2014-11-18 22:03:05 +0800

    implement basic fetch stock symbol mechanism

--- client/app.js
+++ client/app.js
@@ -1,8 +1,26 @@
 var App = React.createClass({
+  getInitialState: function() {
+    return {
+      symbol: undefined,
+    };
+  },
+  getStockData: function(symbol) {
+    this.setState({
+      symbol: symbol
+    });
+  },
   render: function() {
+    var heading;
+    if(this.state.symbol) {
+      heading = <h3>Current stock: {this.state.symbol}</h3>;
+    } else {
+      heading = <h3>No stock selected yet</h3>;
+    }
+
     return (
       <div className='blackbox'>
-        <StockQueryForm />
+        <StockQueryForm onSubmit={this.getStockData}/>
+        {heading}
       </div>
     );
   }
--- client/components/StockQueryForm.js
+++ client/components/StockQueryForm.js
@@ -1,7 +1,12 @@
 var StockQueryForm = React.createClass({
+  submitQuote: function(event) {
+    event.preventDefault();
+    var symbol = $('.stock-query-symbol').val();
+    this.props.onSubmit(symbol);
+  },
   render: function() {
     return (
-      <form className="stock-query">
+      <form className="stock-query" onSubmit={this.submitQuote}>
         <input type='text' className='stock-query-symbol' />
         <input type='submit' />
       </form>
