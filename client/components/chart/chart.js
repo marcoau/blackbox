2ac4433d682b2b18ec88757d152e1b5183b3f1e8
Marco Au <marco.au.321@gmail.com>
2014-11-19 23:04:25 +0800

    add fat stick mode

--- client/components/chart/chart.js
+++ client/components/chart/chart.js
@@ -1,4 +1,9 @@
 var Chart = React.createClass({
+  getInitialState: function() {
+    return {
+      fatStickMode: true
+    }
+  },
   render: function() {
 
     var dateFormat = d3.time.format("%Y-%m-%d");
@@ -27,30 +32,53 @@ var Chart = React.createClass({
         }))
       ])
       .range([height - margin, margin]);
-    var candleWidth = width / data.length;
+
+    if(this.state.fatStickMode) {
+      // calculate candle widths based on volume
+      var totalVolume = _.reduce(data, function(memo, quote){
+        return memo + quote.Volume;
+      }, 0);
+      _.each(data, function(quote, i){
+        quote.width = (width - 2 * margin) * (quote.Volume / totalVolume);
+        if(i){
+          quote.x = data[i-1].x + data[i-1].width;
+        }else{
+          quote.x = 0;
+        }
+      });
+    } else {
+      var candleWidth = width / data.length;
+    }
 
     var candleBodies = _.map(data, function(d, i) {
-      d.x = i ? data[i-1].x + candleWidth : 0;
+      if(!this.state.fatStickMode) {
+        d.x = i ? data[i-1].x + candleWidth : 0;
+      } else {
+        var candleWidth = d.width;
+      }
       var candleX = d.x + margin;
       var candleY = y(Math.max(d.Open, d.Close));
       var candleHeight = y(Math.min(d.Open, d.Close))-y(Math.max(d.Open, d.Close));
-      // var candleWidth = d.width;
       var candleFill = d.Open > d.Close ? "red" : "green";
       return (
         <ChartCandleBody x={candleX} y={candleY} width={candleWidth} height={candleHeight} fill={candleFill} />
       );
-    });
+    }.bind(this));
     var candleStems = _.map(data, function(d, i) {
-      var stemX1 = d.x + candleWidth / 2 + margin;
-      var stemX2 = d.x + candleWidth / 2 + margin;
+      if(this.state.fatStickMode) {
+        var stemX = d.x + d.width / 2 + margin;
+      } else {
+        var stemX = d.x + candleWidth / 2 + margin;
+      }
       var stemY1 = y(d.High);
       var stemY2 = y(d.Low);
       var stemStroke = d.Open > d.Close ? "red" : "green";
       var stemStrokeWidth = candleWidth / 5;
       return (
-        <ChartCandleStem x1={stemX1} x2={stemX2} y1={stemY1} y2={stemY2} stroke={stemStroke} strokeWidth={stemStrokeWidth} />
+        <ChartCandleStem x1={stemX} x2={stemX} y1={stemY1} y2={stemY2} stroke={stemStroke} strokeWidth={stemStrokeWidth} />
       );
-    });
+    }.bind(this));
+    
     return (
       <svg className="stock-chart" width={width} height={height}>
         {candleBodies}
